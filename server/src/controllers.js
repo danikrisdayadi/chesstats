5dcfed359d69547eb1f914488c9fc0e0983d199c
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2022-07-12 23:45:25 +0700

    Configure eslint and prettier

--- server/src/controllers.js
+++ server/src/controllers.js
@@ -1,16 +1,15 @@
-export async function getStats(req, res, next) {
-    try {
-        const user1 = req.params.username1
-        const user2 = req.params.username2
+export async function getStats(req, res) {
+  try {
+    const user1 = req.params.username1
+    const user2 = req.params.username2
 
-        console.log(user1);
-        console.log(user2);
-        // fetch stats for both player
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json("Successful request!");
-    } catch (err) {
-        res.statusCode = 404;
-        res.send(err);
-    }
+    // fetch stats for both player
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json("Successful request!");
+  } catch (err) {
+    res.statusCode = 404;
+    res.send(err);
+  }
+
 }
--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,6 @@
 import express from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import path from 'path';
 import logger from 'morgan';
 import router from './route.js';
-
-const __dirname = path.resolve();
-
-dotenv.config({ path: path.resolve(__dirname, './.env') });
 
 const app = express();
 app.use(logger('dev'));
@@ -16,7 +9,6 @@ app.use(express.json({ limit: '5mb' }));
 // Declare Routes
 app.use('/match', router);
 
-
 app.listen(process.env.PORT || 5000, () => {
-    console.log(`Server has successfully run at port ${process.env.PORT || 5000}`)
+  console.log(`Server has successfully run at port ${process.env.PORT || 5000}`) // eslint-disable-line
 });
