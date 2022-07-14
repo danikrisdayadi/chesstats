22b2b18e0072589b33ec4ef41dfd523b9058d6e3
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2022-07-14 15:03:32 +0700

    Fix styling for navbar

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -1,19 +1,27 @@
 import AppBar from "@mui/material/AppBar";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
-
 import "./NavigationBar.scss";
-import { Toolbar } from "@mui/material";
+import { Container, Toolbar } from "@mui/material";
 
 function NavigationBar() {
   return (
     <Box className="navigation-box">
       <AppBar position="static" className="navigation-bar">
-        <Toolbar style={{ minHeight: "100%" }}>
-          <Button>
-            <img src="/chesstats_navbar_logo.png" alt="logo" className="logo" />
-          </Button>
-        </Toolbar>
+        <Container maxWidth="xl" className="navigation-bar">
+          <Toolbar style={{ minHeight: "30px", height: "100%" }}>
+            <Button>
+              <img
+                src="/chesstats_navbar_logo.png"
+                alt="logo"
+                className="logo"
+              />
+            </Button>
+            <Container sx={{ flexGrow: 1 }}></Container>
+            <h3 className="text">Compare</h3>
+            <h3 className="text">About</h3>
+          </Toolbar>
+        </Container>
       </AppBar>
     </Box>
   );
