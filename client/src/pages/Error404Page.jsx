898008a6dfeb4469d33e5641d318a698f12c903b
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2022-07-14 15:21:48 +0700

    Refactor pages and create 404 error page

---
+++ client/src/pages/Error404Page.jsx
@@ -0,0 +1,12 @@
+import { Container } from "@mui/material";
+
+function Error404Page() {
+  return (
+    <Container align="center" maxWidth="xl">
+      <br />
+      <h1>Error 404: Page Not Found</h1>
+    </Container>
+  );
+}
+
+export default Error404Page;
