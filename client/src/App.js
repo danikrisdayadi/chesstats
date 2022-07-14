e92b61d2379eda87724c7890ec35decf3d3d5880
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2022-07-14 13:40:41 +0700

    Create template for styling later

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import LandingPage from './landing/LandingPage';
 import NavigationBar from './components/NavigationBar';
 import { StyledEngineProvider } from '@mui/material/styles';
+import "./App.scss";
 
 function App() {
   return (
