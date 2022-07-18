f59ef781028f0328fbe8731e74066e44ec41b70e
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2022-07-18 15:27:16 +0700

    Tidy up columns in results page

--- client/src/pages/ResultsPage.jsx
+++ client/src/pages/ResultsPage.jsx
@@ -1,20 +1,47 @@
 import { Container, Grid, Stack } from "@mui/material";
+import { PaddingY } from "../components/Spacing";
 import "./ResultsPage.scss";
 function ResultsPage() {
   return (
-    <Container align="center" maxWidth="xl">
+    <div>
       <h1>Results Page</h1>
-      <ProfileStack />
-      <Grid container></Grid>
-    </Container>
+      <Grid container>
+        <Grid item xs={6} sm={6} md={3}>
+          <Stack align="center">
+            <PaddingY padding={"100px"} />
+            <p>Current Rating</p>
+            <p>Highest Rating</p>
+            <p>Win/Loss Percentage</p>
+            <p>Total Games</p>
+            <p>Tactics Rating</p>
+          </Stack>
+        </Grid>
+        <Grid item xs={6} sm={6} md={3}>
+          <ProfileStack />
+        </Grid>
+        <Grid item xs={6} sm={6} md={3}>
+          <ProfileStack />
+        </Grid>
+        <Grid item xs={6} sm={6} md={3}>
+          <ProfileStack />
+        </Grid>
+      </Grid>
+    </div>
   );
 }
 
 function ProfileStack() {
   return (
-    <Stack>
-      <img src="/chesstats_logo.png" alt="Avatar" className="profile-logo" />
-      <h2>Dani Purwadi</h2>
+    <Stack align="center">
+      <Stack sx={{ height: "200px" }} alignItems="center">
+        <img src="/chesstats_logo.png" alt="Avatar" className="profile-logo" />
+        <h2>Dani Purwadi</h2>
+      </Stack>
+      <p>Current Rating</p>
+      <p>Highest Rating</p>
+      <p>Win/Loss Percentage</p>
+      <p>Total Games</p>
+      <p>Tactics Rating</p>
     </Stack>
   );
 }
