4c24cf3a7aa7655da3156b7cce0bb476455fcdca
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2022-07-19 22:08:18 +0700

    Deleted server package

--- server/src/route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import asyncHandler from 'express-async-handler';
-import { getStats } from './controllers.js';
-
-const router = express.Router();
-
-router.route('/:username1/:username2').get(asyncHandler(getStats))
-
-export default router
