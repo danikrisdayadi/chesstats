f3be443ec6be05778bf548d3aa9ca0fa084c363b
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2022-07-19 13:20:03 +0700

    Fixed compare page mobile responsiveness

--- client/src/pages/ComparePage.jsx
+++ client/src/pages/ComparePage.jsx
@@ -10,31 +10,34 @@ function ComparePage() {
       justifyContent="center"
       className="grid"
     >
-      <Paper elevation={5} align="center" className="username-box">
-        <PaddingY padding={"2vh"} />
-        <h1>Chess.com Username</h1>
-        <p>
-          We noticed that we do not have your chess.com data. Please input your
-          chess.com username below to continue
-        </p>
-        <PaddingY padding={"2vh"} />
-        <Container>
-          <Grid container>
-            <Grid item xs={6} sm={10} justifyContent="flex-end">
-              <TextField
-                fullWidth
-                id="outlined-basic"
-                label="Username"
-                variant="outlined"
-              />
+      <Grid item xs={11} sm={8} md={6}>
+        <Paper elevation={5} align="center" className="username-box">
+          <PaddingY padding={"3vh"} />
+          <img src="/chesstats_navbar_logo.png" alt="logo" className="logo" />
+          <h2>Enter Chess.com Username</h2>
+          <p>
+            We noticed that we do not have your chess.com data. Please input
+            your chess.com username below to continue
+          </p>
+          <PaddingY padding={"2vh"} />
+          <Container>
+            <Grid container>
+              <Grid item xs={9} sm={10} justifyContent="flex-end">
+                <TextField
+                  fullWidth
+                  id="outlined-basic"
+                  label="Username"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item xs={3} sm={2}>
+                <Button>Enter</Button>
+              </Grid>
             </Grid>
-            <Grid item xs={6} sm={2}>
-              <Button>Enter</Button>
-            </Grid>
-          </Grid>
-        </Container>
-        <PaddingY padding={"5vh"} />
-      </Paper>
+          </Container>
+          <PaddingY padding={"5vh"} />
+        </Paper>
+      </Grid>
     </Grid>
   );
 }
