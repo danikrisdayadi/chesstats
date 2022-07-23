3dc11811b31fb9a43ff7547bf3465e7d354bc249
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2022-07-23 11:02:24 +0700

    Fixed off-center animation

--- src/components/LoadingAnimation.jsx
+++ src/components/LoadingAnimation.jsx
@@ -2,13 +2,11 @@ import "./LoadingAnimation.scss";
 
 function LoadingAnimation() {
   return (
-    <div class="parent">
-      <div class="artboard">
-        <div class="domino">
-          <div className="domino-1"></div>
-          <div className="domino-2"> </div>
-          <div className="domino-3"></div>
-        </div>
+    <div class="artboard">
+      <div class="domino">
+        <div className="domino-1"></div>
+        <div className="domino-2"> </div>
+        <div className="domino-3"></div>
       </div>
     </div>
   );
